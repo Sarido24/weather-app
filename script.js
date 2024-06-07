b8c6af574f311224b99fd9333cc7327d0a17eeec
SARIDO_SIMANULLANG <sarido24@gmail.com>
2024-06-07 17:25:07 +0700

    all in one commit

---
+++ script.js
@@ -0,0 +1,439 @@
+const wmo = {
+    "0": {
+      "day": {
+        "description": "Sunny",
+        "image": "http://openweathermap.org/img/wn/01d@2x.png"
+      },
+      "night": {
+        "description": "Clear",
+        "image": "http://openweathermap.org/img/wn/01n@2x.png"
+      }
+    },
+    "1": {
+      "day": {
+        "description": "Mainly Sunny",
+        "image": "http://openweathermap.org/img/wn/01d@2x.png"
+      },
+      "night": {
+        "description": "Mainly Clear",
+        "image": "http://openweathermap.org/img/wn/01n@2x.png"
+      }
+    },
+    "2": {
+      "day": {
+        "description": "Partly Cloudy",
+        "image": "http://openweathermap.org/img/wn/02d@2x.png"
+      },
+      "night": {
+        "description": "Partly Cloudy",
+        "image": "http://openweathermap.org/img/wn/02n@2x.png"
+      }
+    },
+    "3": {
+      "day": {
+        "description": "Cloudy",
+        "image": "http://openweathermap.org/img/wn/03d@2x.png"
+      },
+      "night": {
+        "description": "Cloudy",
+        "image": "http://openweathermap.org/img/wn/03n@2x.png"
+      }
+    },
+    "45": {
+      "day": {
+        "description": "Foggy",
+        "image": "http://openweathermap.org/img/wn/50d@2x.png"
+      },
+      "night": {
+        "description": "Foggy",
+        "image": "http://openweathermap.org/img/wn/50n@2x.png"
+      }
+    },
+    "48": {
+      "day": {
+        "description": "Rime Fog",
+        "image": "http://openweathermap.org/img/wn/50d@2x.png"
+      },
+      "night": {
+        "description": "Rime Fog",
+        "image": "http://openweathermap.org/img/wn/50n@2x.png"
+      }
+    },
+    "51": {
+      "day": {
+        "description": "Light Drizzle",
+        "image": "http://openweathermap.org/img/wn/09d@2x.png"
+      },
+      "night": {
+        "description": "Light Drizzle",
+        "image": "http://openweathermap.org/img/wn/09n@2x.png"
+      }
+    },
+    "53": {
+      "day": {
+        "description": "Drizzle",
+        "image": "http://openweathermap.org/img/wn/09d@2x.png"
+      },
+      "night": {
+        "description": "Drizzle",
+        "image": "http://openweathermap.org/img/wn/09n@2x.png"
+      }
+    },
+    "55": {
+      "day": {
+        "description": "Heavy Drizzle",
+        "image": "http://openweathermap.org/img/wn/09d@2x.png"
+      },
+      "night": {
+        "description": "Heavy Drizzle",
+        "image": "http://openweathermap.org/img/wn/09n@2x.png"
+      }
+    },
+    "56": {
+      "day": {
+        "description": "Light Freezing Drizzle",
+        "image": "http://openweathermap.org/img/wn/09d@2x.png"
+      },
+      "night": {
+        "description": "Light Freezing Drizzle",
+        "image": "http://openweathermap.org/img/wn/09n@2x.png"
+      }
+    },
+    "57": {
+      "day": {
+        "description": "Freezing Drizzle",
+        "image": "http://openweathermap.org/img/wn/09d@2x.png"
+      },
+      "night": {
+        "description": "Freezing Drizzle",
+        "image": "http://openweathermap.org/img/wn/09n@2x.png"
+      }
+    },
+    "61": {
+      "day": {
+        "description": "Light Rain",
+        "image": "http://openweathermap.org/img/wn/10d@2x.png"
+      },
+      "night": {
+        "description": "Light Rain",
+        "image": "http://openweathermap.org/img/wn/10n@2x.png"
+      }
+    },
+    "63": {
+      "day": {
+        "description": "Rain",
+        "image": "http://openweathermap.org/img/wn/10d@2x.png"
+      },
+      "night": {
+        "description": "Rain",
+        "image": "http://openweathermap.org/img/wn/10n@2x.png"
+      }
+    },
+    "65": {
+      "day": {
+        "description": "Heavy Rain",
+        "image": "http://openweathermap.org/img/wn/10d@2x.png"
+      },
+      "night": {
+        "description": "Heavy Rain",
+        "image": "http://openweathermap.org/img/wn/10n@2x.png"
+      }
+    },
+    "66": {
+      "day": {
+        "description": "Light Freezing Rain",
+        "image": "http://openweathermap.org/img/wn/10d@2x.png"
+      },
+      "night": {
+        "description": "Light Freezing Rain",
+        "image": "http://openweathermap.org/img/wn/10n@2x.png"
+      }
+    },
+    "67": {
+      "day": {
+        "description": "Freezing Rain",
+        "image": "http://openweathermap.org/img/wn/10d@2x.png"
+      },
+      "night": {
+        "description": "Freezing Rain",
+        "image": "http://openweathermap.org/img/wn/10n@2x.png"
+      }
+    },
+    "71": {
+      "day": {
+        "description": "Light Snow",
+        "image": "http://openweathermap.org/img/wn/13d@2x.png"
+      },
+      "night": {
+        "description": "Light Snow",
+        "image": "http://openweathermap.org/img/wn/13n@2x.png"
+      }
+    },
+    "73": {
+      "day": {
+        "description": "Snow",
+        "image": "http://openweathermap.org/img/wn/13d@2x.png"
+      },
+      "night": {
+        "description": "Snow",
+        "image": "http://openweathermap.org/img/wn/13n@2x.png"
+      }
+    },
+    "75": {
+      "day": {
+        "description": "Heavy Snow",
+        "image": "http://openweathermap.org/img/wn/13d@2x.png"
+      },
+      "night": {
+        "description": "Heavy Snow",
+        "image": "http://openweathermap.org/img/wn/13n@2x.png"
+      }
+    },
+    "77": {
+      "day": {
+        "description": "Snow Grains",
+        "image": "http://openweathermap.org/img/wn/13d@2x.png"
+      },
+      "night": {
+        "description": "Snow Grains",
+        "image": "http://openweathermap.org/img/wn/13n@2x.png"
+      }
+    },
+    "80": {
+      "day": {
+        "description": "Light Showers",
+        "image": "http://openweathermap.org/img/wn/09d@2x.png"
+      },
+      "night": {
+        "description": "Light Showers",
+        "image": "http://openweathermap.org/img/wn/09n@2x.png"
+      }
+    },
+    "81": {
+      "day": {
+        "description": "Showers",
+        "image": "http://openweathermap.org/img/wn/09d@2x.png"
+      },
+      "night": {
+        "description": "Showers",
+        "image": "http://openweathermap.org/img/wn/09n@2x.png"
+      }
+    },
+    "82": {
+      "day": {
+        "description": "Heavy Showers",
+        "image": "http://openweathermap.org/img/wn/09d@2x.png"
+      },
+      "night": {
+        "description": "Heavy Showers",
+        "image": "http://openweathermap.org/img/wn/09n@2x.png"
+      }
+    },
+    "85": {
+      "day": {
+        "description": "Light Snow Showers",
+        "image": "http://openweathermap.org/img/wn/13d@2x.png"
+      },
+      "night": {
+        "description": "Light Snow Showers",
+        "image": "http://openweathermap.org/img/wn/13n@2x.png"
+      }
+    },
+    "86": {
+      "day": {
+        "description": "Snow Showers",
+        "image": "http://openweathermap.org/img/wn/13d@2x.png"
+      },
+      "night": {
+        "description": "Snow Showers",
+        "image": "http://openweathermap.org/img/wn/13n@2x.png"
+      }
+    },
+    "95": {
+      "day": {
+        "description": "Thunderstorm",
+        "image": "http://openweathermap.org/img/wn/11d@2x.png"
+      },
+      "night": {
+        "description": "Thunderstorm",
+        "image": "http://openweathermap.org/img/wn/11n@2x.png"
+      }
+    },
+    "96": {
+      "day": {
+        "description": "Light Thunderstorms With Hail",
+        "image": "http://openweathermap.org/img/wn/11d@2x.png"
+      },
+      "night": {
+        "description": "Light Thunderstorms With Hail",
+        "image": "http://openweathermap.org/img/wn/11n@2x.png"
+      }
+    },
+    "99": {
+      "day": {
+        "description": "Thunderstorm With Hail",
+        "image": "http://openweathermap.org/img/wn/11d@2x.png"
+      },
+      "night": {
+        "description": "Thunderstorm With Hail",
+        "image": "http://openweathermap.org/img/wn/11n@2x.png"
+      }
+    }
+  }
+
+
+function changeToDark(){
+  document.getElementById('dark').style.display = "none"
+  document.getElementById('light').style.display = "block"
+   document.body.style = "linear-gradient(to right, rgb(51, 51, 51), #0d0f14);"
+}
+function changeToLight(){
+    console.log("KESINI Light");
+    document.getElementById('dark').style.display = "block"
+    document.getElementById('light').style.display = "none"
+   document.body.style= " background: linear-gradient(to right, rgb(195, 193, 193), #5f86e8);"
+}
+
+const loading = document.getElementById("loading")
+let myLat = "";
+let myLong = "";
+const content = document.getElementById("content")
+const inputSearch = document.getElementById('city-input')
+inputSearch.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("but-search").click();
+  }
+});
+
+
+
+
+
+async function fetchData() {
+  loading.style.display = "block"
+  content.style.display = "none"
+ 
+
+  
+  let searchCity = document.getElementById("city-input").value
+//    let searchCity = ""
+  console.log(myLat, myLong, "INI ADALAHHH TEMPAT SAYA");
+  if(searchCity === ""){
+    searchCity = "JAKARTA"
+  }else{
+    searchCity = searchCity.toUpperCase();
+  }
+
+  console.log(searchCity, "NAMA KOTAAA");
+
+  // let searchCity = "jakarta"
+  let date = new Date();
+  date = date.toDateString()
+  try {
+    const responGeo = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${searchCity}`)
+    const responGeoJson = await responGeo.json()
+    // console.log(responGeoJson, "GEOOO");
+    if(!responGeoJson.results){
+      throw  "Terjadi masalah! Cek kembali nama kota atau jaringan anda"
+    }else{
+      const lat = responGeoJson.results[0].latitude;
+      const lon = responGeoJson.results[0].longitude;
+      const responseOpenMeteo = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,is_day,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=Asia%2FBangkok`)
+      const responseOpenMeteoJson = await responseOpenMeteo.json()
+      console.log(responseOpenMeteoJson, "<< response Open Meteo Json")
+          const currentTem = responseOpenMeteoJson.current.temperature_2m
+          const currentTemUnit = responseOpenMeteoJson.current_units.temperature_2m
+          const curCode = responseOpenMeteoJson.current.weather_code.toString()
+          const curDay = responseOpenMeteoJson.current.is_day
+          const curHumidity = responseOpenMeteoJson.current.relative_humidity_2m
+          const curHumidityUnit = responseOpenMeteoJson.current_units.relative_humidity_2m
+          const windSpeed = responseOpenMeteoJson.current.wind_speed_10m
+          const windSpeedUnit = responseOpenMeteoJson.current_units.wind_speed_10m
+          let daily = responseOpenMeteoJson.daily
+          let dailyUnits = responseOpenMeteoJson.daily_units
+          let temMaxUnit = dailyUnits.temperature_2m_min
+          let temMinUnit = dailyUnits.temperature_2m_min
+          let curDesc;
+          let curImg;
+          if(responseOpenMeteoJson.current){
+            content.style.display = "block"
+            loading.style.display = "none"
+            console.log("JALAN KOKKK");
+         
+
+        }
+          if(curDay === 1){
+            console.log("MASUK KESINI");
+            curDesc =  wmo[curCode].day.description
+            curImg = wmo[curCode].day.image
+           
+            
+          }else{
+            console.log("MASUK KESANA")
+            curImg = wmo[curCode].day.image
+            curDesc = wmo[curCode].night.description
+            document.body.style.backgroundImage = "url('https://i.pinimg.com/originals/71/32/d2/7132d24e46182c47ceca0c63470fea70.jpg')";
+            // document.body.style = "background-repeat: none;background-size: none;"
+            document.body.style = "background: linear-gradient(to right, #b487e5, #101114);"
+          }
+          
+          //   console.log("JALAN WOII");
+          document.getElementById("weather-icon").innerHTML = `<img src="${curImg}"/>`
+          document.getElementById('description').innerHTML = curDesc
+          document.getElementById("temperature").innerHTML = `<h1>${currentTem}${currentTemUnit}</h1>`
+          document.getElementById("city-name").innerHTML = searchCity      
+          document.getElementById("time").innerHTML = date    
+          document.getElementById("humidity").innerHTML = `${curHumidity} ${curHumidityUnit}`  
+          document.getElementById("wind-speed").innerHTML =`${windSpeed} ${windSpeedUnit }`     
+          // document.getElementById("wind-speed").innerHTML = windSpeedUnit   
+          document.getElementById("forecast-grid").innerHTML = ""
+          
+          const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+          // let day = weekday[d.getDay()];
+          console.log(daily);
+          for (let i = 0; i < daily.time.length; i++) {
+            let dailyTime = daily.time[i];
+            let temMax = daily.temperature_2m_max[i]
+            let temMin = daily.temperature_2m_min[i]
+
+            let dailyWeatherCode = daily.weather_code[i]
+            const dateObj = new Date(dailyTime)
+            let day = weekday[dateObj.getUTCDay()]
+            // console.log(day);
+            document.getElementById("forecast-grid").innerHTML += `
+            <div class="forecast-item">
+            <p>${day}</p>
+            <img src="${wmo[dailyWeatherCode].night.image}" alt="Weather Icon">
+            <div class="daily-tem">
+              <p style="font-weight:bold;">Max: ${temMax}&nbsp<span>${temMaxUnit}</span></p>
+              <p>Min: ${temMin} <span>${temMinUnit}</span></p>
+            </div>
+            
+            </div>
+            `
+          }
+
+          document.getElementById("error").innerHTML = ""
+          }
+          
+    } catch (error) {
+        console.log(error, "<<< ini error");
+        document.getElementById("error").innerHTML = error
+        document.getElementById("error").style.color = "red"
+  }finally{
+    loading.style.display = "none"
+    
+  }
+
+}
+
+
+
+fetchData()
+
+
+
